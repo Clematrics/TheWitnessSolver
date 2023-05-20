a94bf2c6b31eb6005cf6dc9a98e6bc0902f6f87a
Clematrics <clematrics.studio@gmail.com>
2023-05-20 16:41:58 +0200

    Completed Graph module

--- lib/defs/graph.ml
+++ lib/defs/graph.ml
@@ -3,12 +3,27 @@ type t = { edges : Edges.t; points : CoordSet.t }
 let empty = { edges = Edges.empty; points = CoordSet.empty }
 let edges g = g.edges
 let points g = g.points
-let add_edge e g = { g with edges = Edges.add e g.edges }
+
+let add_edge e g =
+  let p, p' = Edge.get e in
+  let points = CoordSet.(g.points |> add p |> add p') in
+  let edges = Edges.add e g.edges in
+  { points; edges }
 
 let add_edges edges g =
-  { g with edges = Edges.add_seq (List.to_seq edges) g.edges }
+  let points =
+    List.fold_left
+      CoordSet.(
+        fun points e ->
+          let p, p' = Edge.get e in
+          points |> add p |> add p')
+      g.points edges
+  in
+  let edges = List.fold_left (Fun.flip Edges.add) g.edges edges in
+  { points; edges }
 
 let add_point pos g = { g with points = CoordSet.add pos g.points }
+let mem g p = CoordSet.mem p g.points
 let adjacent_edges pos g = g.edges |> Edges.filter (Edge.is_adjacent pos)
 
 let remove pos g =
@@ -17,4 +32,50 @@ let remove pos g =
     points = CoordSet.remove pos g.points;
   }
 
-let fold_points f init g = CoordSet.fold f init g.points
+let arity p g = Edges.cardinal (adjacent_edges p g)
+
+let arity_map g =
+  let update_map = function None -> Some 1 | Some i -> Some (i + 1) in
+  CoordMap.empty
+  |> Edges.fold
+       (fun edge map ->
+         let p, p' = Edge.get edge in
+         map |> CoordMap.update p update_map |> CoordMap.update p' update_map)
+       g.edges
+
+let remove_edge e g =
+  let p, p' = Edge.get e in
+  let update_point p points =
+    if arity p g = 1 then CoordSet.remove p points else points
+  in
+  let edges = Edges.remove e g.edges in
+  let points = g.points |> update_point p |> update_point p' in
+  { points; edges }
+
+let fold_points f g init = CoordSet.fold f g.points init
+
+let fold_edges f g init = Edges.fold f g.edges init
+
+let union { points; edges } g' =
+  {
+    points = CoordSet.union points g'.points;
+    edges = Edges.union edges g'.edges;
+  }
+
+let pp fmt { points; edges } =
+  let pp_set (type a b) (module S : Set.S with type elt = a and type t = b)
+      (pp : _ -> a -> _) fmt (set : S.t) =
+    let open S in
+    let rec pp_elts fmt set =
+      match choose_opt set with
+      | None -> Format.fprintf fmt ""
+      | Some elt ->
+          Format.fprintf fmt "%a@ ;@ %a" pp elt pp_elts (remove elt set)
+    in
+    Format.fprintf fmt "@[<hov 4>{@;%a@;}@]" pp_elts set
+  in
+  Format.fprintf fmt "@[<hov 4>{@;points = %a;@;edges = %a@;}@]"
+    (pp_set (module CoordSet) Coord.pp)
+    points
+    (pp_set (module Edges) Edge.pp)
+    edges
--- lib/defs/graph.mli
+++ lib/defs/graph.mli
@@ -1,18 +1,53 @@
+(** It is garuanteed that both ends of an edge are points contained in the
+    graph. *)
+
 type t
 
 val empty : t
 val edges : t -> Edges.t
 val points : t -> CoordSet.t
+
 val add_edge : Edge.t -> t -> t
+(** [add_edge e g] adds the edge [e] and the ends [p] and [p'] of [e] to [g]. *)
+
 val add_edges : Edge.t list -> t -> t
+(** [add_edges edges g] is equivalent to
+    [List.fold_left (Fun.flip add_edge) g edges]. *)
+
 val add_point : Coord.t -> t -> t
 
 val remove : Coord.t -> t -> t
 (** [remove p g] removes the point [p] and all adjacent edges. *)
 
-val adjacent_edges : Coord.t -> t -> Edges.t
-(** [adjacent_edges p g] returns the set of edges adjacent to [p] in [g] *)
+val remove_edge : Edge.t -> t -> t
+(** [remove e g] removes the edge [e]. If the points adjacent to [e] have an
+    arity of 0 after the edge is removed, they are removed too. *)
 
-val fold_points : (Coord.t -> 'a -> 'a) -> 'a -> t -> 'a
-(** [fold_points f init g] is the same as [f p_n' (f p_n (... (f p_0 init)))].
+val mem : t -> Coord.t -> bool
+(** [mem g p] returns [true] if the point [p] is in [g], and [false] otherwise. *)
+
+val adjacent_edges : Coord.t -> t -> Edges.t
+(** [adjacent_edges p g] returns the set of edges adjacent to [p] in [g]. If the
+    point [p] is not in the graph, [Edges.empty] is returned. *)
+
+val arity : Coord.t -> t -> int
+(** [arity p g] returns the aroty of the point [p] in the graph [g], ie. the
+    number of edges adjacent to [p]. If [p] is not in [g], 0 is returned. This
+    is equivalent to [Edges.cardinal (adjacent_edges p g)] *)
+
+val arity_map : t -> int CoordMap.t
+(** [arity_map g] returns the map of points to integers, where the binding (p,
+    i) is in the map if and only if [p] is in the graph [g] with arity [i]. *)
+
+val fold_points : (Coord.t -> 'a -> 'a) -> t -> 'a -> 'a
+(** [fold_points f g init] is the same as [f p_n' (f p_n (... (f p_0 init)))].
     The order in which points are processed is undefined. *)
+
+val fold_edges : (Edge.t -> 'a -> 'a) -> t -> 'a -> 'a
+(** [fold_edges f g init] is the same as [f e_n' (f e_n (... (f e_0 init)))].
+    The order in which edges are processed is undefined. *)
+
+val union : t -> t -> t
+(** [union g g'] merges two graphs together, by merging their points and edges. *)
+
+val pp : Format.formatter -> t -> unit
