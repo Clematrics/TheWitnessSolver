d715424e16ee284d0d0f2d059014f4a4ffeb9390
Clematrics <clematrics.studio@gmail.com>
2023-05-20 16:50:51 +0200

    Adjusted rendering code to use Graph module Cleaned code

--- lib/render/render.ml
+++ lib/render/render.ml
@@ -29,73 +29,6 @@ let path_from_points ?(rel = false) = function
 let shape_from_points ?(rel = false) p = p |> path_from_points ~rel |> P.close
 let points_from_coords = List.map V2.of_tuple
 
-(* Peut-être factorisé *)
-(* let render_path style board pos elt = function
-   | Path b ->
-       (* TODO: *)
-       let img =
-         P.empty
-         |> P.rect
-              (Box2.v
-                 V2.(zero - (v path_width path_width / 2.))
-                 V2.(v path_width path_width))
-         |> Fun.flip I.cut (I.const style#navigation_color)
-       in
-       let img =
-         if List.length elt.connected_paths = 2 then
-           let x, y =
-             List.fold_left
-               (fun o -> o |> Coord.( +: ))
-               (0, 0) elt.connected_paths
-           in
-           let c =
-             V2.( * ) (path_width /. 2.) (V2.v (float_of_int x) (float_of_int y))
-           in
-           I.cut (P.empty |> P.circle c path_width) img
-         else img
-       in
-       let img = if b then img else I.void in
-       List.fold_left
-         (fun img o ->
-           match CoordMap.find_opt Coord.(pos +: o) board with
-           | Some { path = Some (Path _); _ } | Some { path = Some (Start _); _ }
-             ->
-               let cell_half_inner = (cell_size /. 2.) -. path_width in
-               let dx, dy = o in
-               let dx, dy = (float_of_int dx, float_of_int dy) in
-               let sx, sy =
-                 ( (dx *. cell_half_inner) -. (dy *. path_width),
-                   (dy *. cell_half_inner) +. (dx *. path_width) )
-               in
-               let dx, dy = (dx +. dy, dy -. dx) in
-               let path =
-                 P.empty
-                 |> P.rect
-                      (Box2.v
-                         V2.(mul (v dx dy) (v path_width path_width / 2.))
-                         V2.(v sx sy))
-               in
-               I.cut path (I.const style#navigation_color) |> I.blend img
-           | _ -> img)
-         img elt.connected_paths
-   | Start b ->
-       let circ = P.circle V2.zero path_width P.empty in
-       I.cut circ
-         (I.const (if b then style#navigation_color else style#disabled_color))
-   | End _ ->
-       let dx, dy = List.hd elt.connected_paths in
-       let path =
-         [
-           ( cell_size /. 2. *. float_of_int dx,
-             cell_size /. 2. *. float_of_int dy );
-           ( cell_size /. 4. *. float_of_int dx,
-             cell_size /. 4. *. float_of_int dy );
-         ]
-         |> points_from_coords |> path_from_points
-       in
-       let area = `O { P.o with width = path_width; cap = `Round } in
-       I.cut ~area path (I.const style#navigation_color) *)
-
 let img_from_shape color shape r =
   let bsize = 0.9 in
   let block (i, j) =
@@ -242,44 +175,21 @@ let pp_list pp fmt l =
   Format.fprintf fmt "[@[ %a @]]" inner l
 
 (* TODO: bad junction when end is perpendicular to a single path *)
-let paths_layer style { points; cuts; edges; ends; _ } =
-  let arity_map =
-    CoordMap.empty
-    |> (* Adding junctions with their arity *)
-    CoordSet.fold
-      (fun pos ->
-        edges
-        |> Edges.filter (Edge.is_adjacent pos)
-        |> Edges.cardinal |> CoordMap.add pos)
-      (CoordSet.union points cuts)
-  in
-  (* decrease arity of points adjacent to the edge given *)
-  let decrease_arity edge map =
-    (* decrease arity of a point *)
-    let decrease_arity pos =
-      CoordMap.update pos (function
-        | None | Some 0 -> None
-        | Some i -> Some (i - 1))
-    in
-    let c, c' = Edge.get edge in
-    map |> decrease_arity c |> decrease_arity c'
-  in
-  let rec synthetize_path complete_path arity_map edges =
-    (* Format.printf "Synthesizing %a\n" (pp_list pp_coords) complete_path; *)
-    let path = start_path arity_map edges in
-    let hole_size = (cell_size -. path_width) /. 3. in
+let paths_layer style { graph; cuts_graph; ends; _ } =
+  let is_cut p = (not (Graph.mem graph p)) && Graph.mem cuts_graph p in
+  let rec synthetize_path complete_path graph =
+    let path = start_path graph in
+    let hole_size = (cell_size -. path_width) /. 5. in
     let rec iter_points ?(is_first = false) acc = function
       | [] -> [ acc ]
-      | c :: c' :: l when CoordSet.mem c cuts ->
-          (* Format.printf "Found a cut at %a\n" pp_coords c; *)
+      | c :: c' :: l when is_cut c ->
           let p, p' = (v2 c, v2 c') in
           let dir = V2.(p' - p) in
           acc
           :: iter_points
                [ V2.(p + (hole_size /. 2. * dir / norm dir)) ]
                (c' :: l)
-      | c :: c' :: l when CoordSet.mem c' cuts ->
-          (* Format.printf "Found a cut at %a\n" pp_coords c'; *)
+      | c :: c' :: l when is_cut c' ->
           let p, p' = (v2 c, v2 c') in
           let dir = V2.(p' - p) in
           (V2.(p' - (hole_size /. 2. * dir / norm dir)) :: p :: acc)
@@ -287,12 +197,21 @@ let paths_layer style { points; cuts; edges; ends; _ } =
       | c :: c' :: l when IntMap.exists (fun _ -> ( = ) c) ends ->
           let p, p' = (v2 c, v2 c') in
           let dir = V2.(p' - p) in
+          assert (acc = []);
           iter_points [ V2.(p + (cell_size /. 2. * dir / norm dir)) ] (c' :: l)
       | c :: c' :: l when IntMap.exists (fun _ -> ( = ) c') ends ->
           let p, p' = (v2 c, v2 c') in
           let dir = V2.(p' - p) in
           (V2.(p' - (cell_size /. 2. * dir / norm dir)) :: p :: acc)
           :: iter_points [] l
+      | [ c; c' ] when is_first ->
+          let p, p' = (v2 c, v2 c') in
+          let dir = V2.(p' - p) in
+          iter_points
+            (V2.(p' + (path_width /. cell_size * dir / norm dir))
+            :: V2.(p - (path_width /. cell_size * dir / norm dir))
+            :: acc)
+            []
       | c :: c' :: l when is_first ->
           let p, p' = (v2 c, v2 c') in
           let dir = V2.(p' - p) in
@@ -317,14 +236,10 @@ let paths_layer style { points; cuts; edges; ends; _ } =
                List.fold_left (Fun.flip P.line) path l
            | [] -> path)
          path
-  and iter_from_path ({ completed; last_edge; _ } as path) arity_map edges =
-    (* Format.printf "Go through edge %a to %a\n" Edge.pp last_edge Coord.pp
-       (List.hd completed); *)
+  and iter_from_path ({ completed; last_edge; _ } as path) graph =
     (* try to find the next edge in the same direction as last_edge *)
     (* possible edges to cross *)
-    let candidate_edges =
-      edges |> Edges.filter (Edge.is_adjacent (List.hd completed))
-    in
+    let candidate_edges = Graph.adjacent_edges (List.hd completed) graph in
     (* edge opposite to the last one crossed, if in candidates *)
     let opposite_edge =
       candidate_edges
@@ -335,30 +250,28 @@ let paths_layer style { points; cuts; edges; ends; _ } =
     let edge = orr opposite_edge (Edges.min_elt_opt candidate_edges) in
     match edge with
     | Some edge ->
-        (* Format.printf "Found edge %a to continue\n" Edge.pp edge; *)
         let next_point = Edge.other_end edge (List.hd completed) in
-        (if CoordMap.mem next_point arity_map then
+        (if Graph.mem graph next_point then
            iter_from_path
              { path with completed = next_point :: completed; last_edge = edge }
          else synthetize_path (next_point :: completed))
-          (decrease_arity edge arity_map)
-          (Edges.remove edge edges)
+          (Graph.remove_edge edge graph)
     | None ->
         (* No edges left to continue the path *)
-        synthetize_path completed arity_map edges
-  and start_path arity_map edges =
+        synthetize_path completed graph
+  and start_path graph =
     (* select a path that has only one edge first *)
     match
-      arity_map |> CoordMap.filter (fun _ -> ( = ) 1) |> CoordMap.choose_opt
+      Graph.arity_map graph
+      |> CoordMap.filter (fun _ -> ( = ) 1)
+      |> CoordMap.choose_opt
     with
     | Some (pos, _) ->
         (* Format.printf "Start by junction at %a\n" pp_coords pos; *)
-        let edge =
-          edges |> Edges.filter (Edge.is_adjacent pos) |> Edges.min_elt
-        in
+        let edge = Graph.adjacent_edges pos graph |> Edges.min_elt in
         let pos' = Edge.other_end edge pos in
         (* check path exists on the other size *)
-        (if CoordMap.mem pos' arity_map then
+        (if Graph.mem graph pos' then
            iter_from_path
              ({
                 start_by_path = true;
@@ -367,22 +280,20 @@ let paths_layer style { points; cuts; edges; ends; _ } =
               }
                : incomplete_path)
          else synthetize_path [ pos'; pos ])
-          (decrease_arity edge arity_map)
-          (Edges.remove edge edges)
+          (Graph.remove_edge edge graph)
     | None -> (
         (* TODO: select a path with more than one edge *)
         (* select an edge otherwise *)
-        match edges |> Edges.min_elt_opt with
+        match Graph.edges graph |> Edges.min_elt_opt with
         | Some edge ->
-            (* Format.printf "Start by edge %a\n" pp_edge edge; *)
             (* select a point in a direction to go to. c is the start, c' the current end *)
             let c, c' = Edge.get edge in
             let c, c' =
-              if CoordMap.mem c arity_map then (c', c)
+              if Graph.mem graph c then (c', c)
                 (* choose c as the current point *)
               else (c, c')
             in
-            (if CoordMap.mem c arity_map || CoordMap.mem c' arity_map then
+            (if Graph.mem graph c || Graph.mem graph c' then
                iter_from_path
                  {
                    start_by_path = false;
@@ -392,14 +303,13 @@ let paths_layer style { points; cuts; edges; ends; _ } =
              else
                (* Edge disconnected from both sides *)
                synthetize_path [ c'; c ])
-              (decrease_arity edge arity_map)
-              (Edges.remove edge edges)
+              (Graph.remove_edge edge graph)
         | None ->
             (* Printf.printf "Starting nowhere, no edge found\n"; *)
             P.empty
             (* No other paths can be created *))
   in
-  let path = start_path arity_map edges in
+  let path = start_path (Graph.union graph cuts_graph) in
   let area = `O { P.o with width = path_width; join = `Round } in
   I.cut ~area path (I.const style#navigation_color)
 
@@ -413,12 +323,11 @@ let start_layer style starts =
       |> I.blend)
     starts I.void
 
-let end_layer style { ends; edges; _ } =
+let end_layer style { ends; graph; _ } =
   IntMap.fold
     (fun _ pos ->
       let pos' =
-        edges
-        |> Edges.filter (Edge.is_adjacent pos)
+        Graph.adjacent_edges pos graph
         |> Edges.choose
         |> Fun.flip Edge.other_end pos
       in
